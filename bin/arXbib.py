4f4ac840b6249a6b985e172be825b3d498611c95
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-01-05 22:50:56 +0100

    clear trailing whitespaces

--- bin/arXbib.py
+++ bin/arXbib.py
@@ -22,25 +22,25 @@ def main(argv) :
 
     try :
         opts, args = getopt.getopt(argv,"o:k:f")
-    except getopt.GetoptError : 
+    except getopt.GetoptError :
         print(helpstring)
         exit(1)
-        
+
     if len(args)==0 :
         print(helpstring)
         exit(2)
-        
+
     for opt,arg in opts :
         if opt=='-o' : bibfile = arg
         elif opt=='-k' : key = arg
         elif opt=='-f' : force = True
-        
-    ID=args[0]    
 
-        
+    ID=args[0]
+
+
     if ID=="" :
         print("No arXiv ID given");
-        exit(2)    
+        exit(2)
 
     # assemble INSPIRE url to search for this article
     url = "http://inspirehep.net/search?p=find+eprint+{}".format(ID)
@@ -53,11 +53,11 @@ def main(argv) :
     #print(web_page)
     soup = BeautifulSoup(web_page)
 
-    # browse to the first search entry and extract links to bib files 
+    # browse to the first search entry and extract links to bib files
     entries= soup.find("div",attrs={'class' : 'record_body'})
     l=entries.find("ul",attrs={'class' : 'tight_list'})
     links = l.findAll("a")
-    biburl = [a['href'] for a in links if a.text=="BibTeX"]  
+    biburl = [a['href'] for a in links if a.text=="BibTeX"]
 
     #print(biburl)
 
@@ -65,50 +65,50 @@ def main(argv) :
     bib_page= urlrequest.urlopen(biburl[0]).read()
     bibsoup = BeautifulSoup(bib_page)
     bibtex = bibsoup.find("pre")
-    
+
     # check that the eprint ID is the same as the one you requested
     lines=bibtex.text.splitlines()
     for line in lines :
-        if "eprint" in line and not ID in line : 
+        if "eprint" in line and not ID in line :
             print(bcolors.FAIL+"Entry eprint ID differs from requested one!"+bcolors.ENDC)
             exit(3)
 
     # get (and possibly replace) article bibID
     keyline=[(i,line) for i,line in enumerate(lines) if "@article" in line][0]
     bibID = keyline[1][9:-1]
-   
+
     if key!="" :
         lines[keyline[0]]=lines[keyline[0]].replace(bibID,key)
-    else : key=bibID     
+    else : key=bibID
 
     # print bibtex entry to the console
     for line in lines :
         print(line)
 
 
-    # if bibfile is given, patch the bibfile        
+    # if bibfile is given, patch the bibfile
     if bibfile!="" :
         # check if entry is already present
         # http://stackoverflow.com/questions/82831/check-whether-a-file-exists-using-python
         import os.path
         if os.path.isfile(bibfile):
             biblio=open(bibfile,'r').read()
-            if key in biblio : 
+            if key in biblio :
                 print(bcolors.FAIL+"bibtex key {} already in use! \nDid not patch bib file.".format(key) + bcolors.ENDC)
                 exit(4) # exit after duplicate key has been found
-            if ID in biblio : 
-                print(bcolors.WARNING+"An article with the same arXive ID is already in the bibliography"+bcolors.ENDC)   
-                if not force : 
+            if ID in biblio :
+                print(bcolors.WARNING+"An article with the same arXive ID is already in the bibliography"+bcolors.ENDC)
+                if not force :
                     print(bcolors.WARNING+"Force entry with -f option."+bcolors.ENDC)
                     exit(5)
             accessmode = "a"
         else:
             accessmode = "w"
-                
+
         with open(bibfile, accessmode) as f:
             for line in lines : f.write(line+"\n")
-                      
-        print(bcolors.OKGREEN+"Added bibtex entry with key {}.".format(key) + bcolors.ENDC)  
+
+        print(bcolors.OKGREEN+"Added bibtex entry with key {}.".format(key) + bcolors.ENDC)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
